382250ca07f018e5f408c06806d2982e90c8c810
keitepassos <keitepassos@gmail.com>
2019-09-16 22:23:31 -0300

    up2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'people', loadChildren: './people/people.module#PeoplePageModule' },
+  { path: 'films', loadChildren: './films/films.module#FilmsPageModule' },
+  { path: 'startships', loadChildren: './startships/startships.module#StartshipsPageModule' },
+  { path: 'planets', loadChildren: './planets/planets.module#PlanetsPageModule' }
 ];
 
 @NgModule({
---
+++ src/app/startships/startships.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startships',
+  templateUrl: './startships.page.html',
+  styleUrls: ['./startships.page.scss'],
+})
+export class StartshipsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
