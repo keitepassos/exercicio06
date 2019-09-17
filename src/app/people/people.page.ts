937c2951da4ae40557eea0f3be3a3db5b4998d3f
Rodolpho  Bruno <rodolphobruno@MacBook-Air-de-Rodolpho.local>
2019-09-17 01:16:49 -0300

    conf json api

--- src/app/people/people.page.ts
+++ src/app/people/people.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-people',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeoplePage implements OnInit {
 
-  constructor() { }
+  json : string;
+  persons : string;
+  constructor(private http : HTTP) { 
+    this.consultarPeople();
+  }
+
+  consultarPeople(){
+    let url : string = "https://swapi.co/api/people/?format=json"
+    console.log("Chamando "+url)
+    this.http.get(url,{},{})
+      .then(data => {
+        this.json = data.data;
+      })
+      .catch(error => {
+        console.log(error.status);
+        console.log(error.error);
+        console.log(error.headers);
+      });   
+  }
 
   ngOnInit() {
   }
